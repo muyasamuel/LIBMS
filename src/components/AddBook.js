7c5349cc3a84fced13ce30c5194b527f705fefa9
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-09 10:41:54 +0300

    Replace an input with select on the category filing

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,21 +1,26 @@
-import './AddBook.css'
+import "./AddBook.css";
 
 const AddBook = () => {
   return (
-    <div className='addContainer'>
-        <div className='addWrapper'>
+    <div className="addContainer">
+      <div className="addWrapper">
         <h2>Add New Book</h2>
         <form>
-            <input type='text'  placeholder='Book title '/>
-            <input type='text'  placeholder='Authors Name '/>
-            <input type='number'  placeholder='Amount '/>
-            <input type='text'  placeholder='Category '/>
-            <button>Submit</button>
-        </form>
-        </div>
+          <input type="text" placeholder="Book title " />
+          <input type="text" placeholder="Authors Name " />
+          <input type="number" placeholder="Amount " />
+          <label for="cars">Select category:</label>
+          <select name="books" id="books">
+            <option value="Novel">Novel</option>
+            <option value="Revision book">Revision Book</option>
+            <option value="Course Work">Course Work</option>
         
+          </select>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddBook
+export default AddBook;
