818ed6f23069b00b5b52c86faea988610b650aa4
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-09 09:47:29 +0300

    Adding a register from through the register comp

---
+++ src/components/Register.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='formContainer'> 
+     <div className='formWrapper'>
+        <h1>Login</h1>
+        <form >
+        <input  type='text' placeholder='Enter name' />
+        <input  type='password' placeholder='Password' />
+        <button>Login</button>
+        </form>
+        
+     </div>
+        
+        
+    </div>
+  )
+}
+
+export default Register
