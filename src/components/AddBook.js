4a7785a128533b4dcf2f763a87094dec2ec3c336
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-11 04:47:49 +0300

    adding error state to the inputs

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -7,18 +7,35 @@ const AddBook = () => {
    const [amount , setAmount ] = useState(0);
    const [category , setCategory ] = useState('');
 
+   const [amountInvalid , setAmountInvalid] = useState(false);
+   const [namesInvalid, setNamesInvalid] = useState(false);
+
 
    const submitHandler = (e) =>{
     e.preventDefault();
+ 
+    if(amount < 1){
+      setAmountInvalid(true);
+       
+    }else if(title.trim() === "" || author.trim() === ""   ){
+      setNamesInvalid(true);
+    }else{
+      const newEntry = {
+        title,
+        author,
+        amount,
+        category
+      }
+      console.log(newEntry);
 
-    const newEntry = {
-      title,
-      author,
-      amount,
-      category
     }
 
-    console.log(newEntry);
+  
+    setTitle("");
+    setAuthor("");
+    setAmount("");
+    setCategory("");
+    
    }
 
   return (
@@ -26,6 +43,8 @@ const AddBook = () => {
       <div className="addWrapper">
         <h2>Add New Book</h2>
         <form onSubmit={submitHandler}>
+          {amountInvalid && <p className="error">Please fill in a proper amount</p>}
+          {namesInvalid && <p className="error">Please fill in names</p>}
           <input type="text" placeholder="Book title "  value={title} onChange={(e)=> setTitle(e.target.value)} />
           <input type="text" placeholder="Authors Name " value={author} onChange={(e)=> setAuthor(e.target.value)}  />
           <input type="number" placeholder="Amount " value={amount} onChange={(e)=> setAmount(e.target.value)}  />
