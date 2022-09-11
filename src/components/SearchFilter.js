1b2f8fd7ae0179e369686f0caee800024a13c52a
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-11 21:03:06 +0300

    Styling the derchFilter

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import './SearchFilter.css'
 
 function SearchFilter() {
   return (
-    <div>
+    <div className='searchContainer'>
+       
+        <div className='wrapper'>
+            <div className='inputDiv'> <input type='text'  placeholder='search..'/></div>
+            
+            <div className='content'> books content</div>
+        </div>
+        
 
     </div>
   )
