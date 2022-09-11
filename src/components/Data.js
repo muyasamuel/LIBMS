829e60b1c13ef1f1b2e7bd69de8ab018cf8ec4dc
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-11 21:12:26 +0300

    Dummy data on the Available books

---
+++ src/components/Data.js
@@ -0,0 +1,58 @@
+ export const booksAvailable = [
+    {
+        id: 1,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Novel'
+    },
+    {
+        id: 2,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Revision book'
+    },
+    {
+        id: 3,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Course Work'
+    },
+    {
+        id: 4,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Novel'
+    },
+    {
+        id: 5,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Revision book'
+    },
+    {
+        id: 6,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Course Work'
+    },
+    {
+        id: 7,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Novel'
+    },
+    {
+        id: 8,
+        title: 'River and Source',
+        author: 'Margaret Ogola',
+        amount: 5,
+        category: 'Revision book'
+    }
+]
