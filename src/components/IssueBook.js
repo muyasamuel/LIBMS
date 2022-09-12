8c34322e262f3c70ad4e011b5551947f193cadda
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-12 22:50:11 +0300

    add reset method to empty values after submit

--- src/components/IssueBook.js
+++ src/components/IssueBook.js
@@ -6,10 +6,12 @@ function IssueBook() {
     register,
     handleSubmit,
     formState: { errors },
+    reset
   } = useForm();
 
   const submitHandler = (data) => {
     console.log(data);
+    reset();
   };
 
   return (
@@ -21,66 +23,78 @@ function IssueBook() {
           <form onSubmit={handleSubmit(submitHandler)}>
             <div>
               <label>
-              Students Name :
-              <input
-                type="text"
-                name="name"
-                placeholder="Students Name"
-                {...register("name", { required: true, maxLength: 7 })}
-              />
-              
-            </label>
-            {errors.name && <p style={{color: 'red'}}> Please check on the inputed name</p>}
+                Students Name :
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Students Name"
+                  {...register("name", { required: true, maxLength: 7 })}
+                />
+              </label>
+              {errors.name && (
+                <p style={{ color: "red" }}>
+                  {" "}
+                  Please check on the inputed name
+                </p>
+              )}
             </div>
             <div>
               <label>
-              Students Email :
-              <input
-                type="email"
-                name="email"
-                placeholder="Students Email"
-                {...register("email", {
-                  required: true,
-                  
-                })}
-              
-              />
-            
-            </label>
-            {errors.email && <p style={{color: 'red'}}>Please put n a valid email</p>}
+                Students Email :
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Students Email"
+                  {...register("email", {
+                    required: true,
+                  })}
+                />
+              </label>
+              {errors.email && (
+                <p style={{ color: "red" }}>Please put n a valid email</p>
+              )}
             </div>
             <div>
-               <label>
-              Book Title :
-              <input
-                type="text"
-                name="title"
-                placeholder="Book Title"
-                {...register("title", { required: true })}
-               
-              />
-              
-            </label>
-            {errors.title && <p style={{color: 'red'}}> Please fill in the book title</p>}
+              <label>
+                Book Title :
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Book Title"
+                  {...register("title", { required: true })}
+                />
+              </label>
+              {errors.title && (
+                <p style={{ color: "red" }}> Please fill in the book title</p>
+              )}
             </div>
-           <div>
-             <label>
-              Issued Date :
-              <input type="date" name="issued date" {...register("issuedDate" , { required: true})} />
-             
-            </label>
-            {errors.issuedDate && <p style={{color: 'red'}}> Please put in a date</p>}
-           </div>
-           <div>
-             <label>
-              Return Date :
-              <input type="date" name="return date" {...register("returnDate" , {required: true})} />
-              
-            </label>
-            {errors.issuedDate && <p style={{color: 'red'}}> Please put in a date</p>}
+            <div>
+              <label>
+                Issued Date :
+                <input
+                  type="date"
+                  name="issued date"
+                  {...register("issuedDate", { required: true })}
+                />
+              </label>
+              {errors.issuedDate && (
+                <p style={{ color: "red" }}> Please put in a date</p>
+              )}
+            </div>
+            <div>
+              <label>
+                Return Date :
+                <input
+                  type="date"
+                  name="return date"
+                  {...register("returnDate", { required: true })}
+                />
+              </label>
+              {errors.issuedDate && (
+                <p style={{ color: "red" }}> Please put in a date</p>
+              )}
+            </div>
 
-           </div>
-           
             <button>Submit </button>
           </form>
         </div>
