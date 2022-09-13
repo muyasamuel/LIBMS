018ae9236ffef854db0dbec8535f4403c33b4275
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-13 15:44:28 +0300

    Add a useContext for state to add or remover editcart

--- src/store/editContext.js
+++ src/store/editContext.js
@@ -1,10 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const editContext = createContext();
 
 const EditProvider = ({children}) => {
+   const [ editCartSeen, setEditCartSeen] =  useState(false);
+
+   const showEditCart = () => {
+    setEditCartSeen(true);
+   };
+
+   const removeEditCart = () => {
+    setEditCartSeen(false);
+   }
+
+
     return (
-        <editContext.Provider>
+        <editContext.Provider value={{
+            editCartSeen,
+            showEditCart,
+            removeEditCart
+
+        }}>
             {children}
         </editContext.Provider>
     )
