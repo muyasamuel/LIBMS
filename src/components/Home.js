1568efce864cd23a947626265e944aeb8a9f1ca3
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-15 20:28:41 +0300

    adding and styling buttons to the home page comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,22 @@ function Home() {
     <div className='homeContainer'>
         <nav>
         <h1><span>LIB</span>MS</h1>
-        <button>Login</button>
+        <div>
+        <button className='navBtn'> Sign Up</button>
+        <button  className='navBtn'>Login</button>
+        </div>
+       
         </nav>
+
+        <main>
+          <h1>Automate <br></br>Your <span>library</span>  <br></br>to Fully Digital one...</h1>
+          <div className='btnDiv'>
+            <button className='mainBtn'> Sign Up</button>
+            <button className='mainBtn' >Login</button>
+          </div>
+         
+
+        </main>
        
     </div>
   )
