e65da18dcebad59f27a36e6eca7cae3861634024
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-20 19:18:38 +0300

    Adding the html for the forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,59 @@ function Form() {
   return (
     <div className="containerWrapper">
       <div className="container">
-        <div className="login-form">
-         </div>
-        <div className="signup-form"></div>
+        <div class="forms">
+          <div class="form-content">
+            <div class="login-form">
+              <div class="title">Login</div>
+              <form action="#">
+                <div class="input-boxes">
+                  <div class="input-box">
+                    <i class="fas fa-envelope"></i>
+                    <input type="text" placeholder="Enter your email" />
+                  </div>
+                  <div class="input-box">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" placeholder="Enter your password" />
+                  </div>
+                  <div class="text">
+                    <button>Forgot your Password</button>
+                  </div>
+                  <div class="button input-box">
+                    <input type="submit" value="Sumbit" />
+                  </div>
+                  <div class="text sign-up-text">
+                    Don't have an account? <label for="flip">Sigup now</label>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="signup-form">
+              <div class="title">Signup</div>
+              <form action="#">
+                <div class="input-boxes">
+                  <div class="input-box">
+                    <i class="fas fa-user"></i>
+                    <input type="text" placeholder="Enter your name" />
+                  </div>
+                  <div class="input-box">
+                    <i class="fas fa-envelope"></i>
+                    <input type="text" placeholder="Enter your email" />
+                  </div>
+                  <div class="input-box">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" placeholder="Enter your password" />
+                  </div>
+                  <div class="button input-box">
+                    <input type="submit" value="Sumbit" />
+                  </div>
+                  <div class="text sign-up-text">
+                    Already have an account? <label for="flip">Login now</label>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
