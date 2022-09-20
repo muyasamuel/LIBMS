ac988e7c2fe2c492be9ff57f9e4f91511f86fd19
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-20 22:38:56 +0300

    Handle errors in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,90 +3,163 @@ import { useForm } from "react-hook-form";
 import { BsFillPersonPlusFill } from "react-icons/bs";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { FaEnvelope } from "react-icons/fa";
-import frontImage from '../images/bg4.jpg';
-import backImage from '../images/bg5.jpg';
-
+import frontImage from "../images/bg4.jpg";
+import backImage from "../images/bg5.jpg";
 
 function Form() {
-    const { register, handleSubmit , formState : { errors}, reset} = useForm();
-    const { register: register2, formState: { errors: errors2 }, handleSubmit: handleSubmit2,   } =  useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const {
+    register: register2,
+    formState: { errors: errors2 },
+    handleSubmit: handleSubmit2,
+    reset: reset2
+  } = useForm();
 
+  const emailPattern =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-   const loginHandler = (data) =>{
+  const loginHandler = (data) => {
     console.log(data);
     reset();
-   }
+  };
 
-   const signUpHandler = (data) => {
+  const signUpHandler = (data) => {
     console.log(data);
-    reset();
-   }
+    reset2();
+  };
   return (
     <div className="containerWrapper">
       <div className="container">
-      <input type="checkbox" id="flip" />
-    <div class="cover">
-      <div class="front">
-         <img src={frontImage}  alt=""/>
-         
-        
-      </div>
-      <div class="back">
-        <img class="backImg" src={backImage} alt=""/>
-        
-      </div>
-    </div>
-        <div class="forms">
-          <div class="form-content">
-            <div class="login-form">
-              <div class="title">Login</div>
+        <input type="checkbox" id="flip" />
+        <div className="cover">
+          <div className="front">
+            <img src={frontImage} alt="" />
+          </div>
+          <div className="back">
+            <img className="backImg" src={backImage} alt="" />
+          </div>
+        </div>
+        <div className="forms">
+          <div className="form-content">
+            <div className="login-form">
+              <div className="title">Login</div>
               <form action="#" onSubmit={handleSubmit(loginHandler)}>
-                <div class="input-boxes">
-                  <div class="input-box">
-                  <FaEnvelope style={{width: '25px', height: '25px'}} />
-                    <input type="text" placeholder="Enter your email"  />
+                <div className="input-boxes">
+                  <div className="input-box">
+                    <FaEnvelope style={{ width: "25px", height: "25px" }} />
+                    <input
+                      type="text"
+                      placeholder="Enter your email"
+                      name="email"
+                      {...register("email", {
+                        required: true,
+                        pattern: emailPattern,
+                      })}
+                    />
                   </div>
-                  <div class="input-box">
-                  <RiLockPasswordLine style={{width: '30px', height: '30px'}} />
-                    <input type="password" placeholder="Enter your password"  name="password" {...register("password", {required: true, pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/ })} />
-                    {errors.password && <p style={{color: 'red'}}> Please fill in a correct password </p>}
+                  {errors.email && (
+                    <p style={{ color: "red" }}> Please fill in an email </p>
+                  )}
+                  <div className="input-box">
+                    <RiLockPasswordLine
+                      style={{ width: "30px", height: "30px" }}
+                    />
+                    <input
+                      type="password"
+                      placeholder="Enter your password"
+                      name="password"
+                      {...register("password", {
+                        required: true,
+                        pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/,
+                      })}
+                    />
                   </div>
-                  <div class="text">
+                  {errors.password && (
+                    <p style={{ color: "red" }}>
+                      Please fill in a correct password{" "}
+                    </p>
+                  )}
+                  <div className="text">
                     <a href="signup">forgot your password</a>
                   </div>
-                  <div class="button input-box">
+                  <div className="button input-box">
                     <input type="submit" value="Submit" />
                   </div>
-                  <div class="text sign-up-text">
-                    Don't have an account? <label for="flip">Sigup now</label>
+                  <div className="text sign-up-text">
+                    Don't have an account? <label htmlFor="flip" >Sigup now</label>
                   </div>
                 </div>
               </form>
             </div>
-            <div class="signup-form">
-              <div class="title">Signup</div>
-              <form action="#" onSubmit={handleSubmit2(signUpHandler)}>
-                <div class="input-boxes">
-                  <div class="input-box">
-                    <BsFillPersonPlusFill style={{width: '30px', height: '30px'}} />
-                    <input type="text" placeholder="Enter your name" name="name" {...register2("name" , {required: true , minLength: 5, maxLength: 12})}  />
-                    {errors2.name && <p style={{color: 'red'}}> Please fill in a name </p>}
+            <div className="signup-form">
+              <div className="title">Signup</div>
+              <form  onSubmit={handleSubmit2(signUpHandler)}>
+                <div className="input-boxes">
+                  <div className="input-box">
+                    <BsFillPersonPlusFill
+                      style={{ width: "30px", height: "30px" }}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Enter your name"
+                      name="name"
+                      {...register2("name", {
+                        required: true,
+                        minLength: 5,
+                        maxLength: 12,
+                      })}
+                    />
+                  </div>
+                  {errors2.name && (
+                    <p style={{ color: "red" }}> Please fill in a name </p>
+                  )}
 
+                  <div className="input-box">
+                    <FaEnvelope style={{ width: "25px", height: "25px" }} />
+                    <input
+                      type="text"
+                      placeholder="Enter your email"
+                      name="email"
+                      {...register2("email", {
+                        required: true,
+                        pattern: emailPattern,
+                      })}
+                    />
                   </div>
-                  <div class="input-box">
-                    <FaEnvelope  style={{width: '25px', height: '25px'}} />
-                    <input type="text" placeholder="Enter your email" />
+                  {errors2.email && (
+                    <p style={{ color: "red" }}>
+                      Please fill in a valid email
+                    </p>
+                  )}
+                  <div className="input-box">
+                    <RiLockPasswordLine
+                      style={{ width: "30px", height: "30px" }}
+                    />
+                    <input
+                      type="password"
+                      placeholder="Enter your password"
+                      name="signupPassword"
+                      {...register2("signupPassword", {
+                        required: true,
+                        pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/,
+                      })}
+                    />
                   </div>
-                  <div class="input-box">
-                  <RiLockPasswordLine style={{width: '30px', height: '30px'}} />
-                    <input type="password" placeholder="Enter your password" name="signupPassword" {...register2("signupPassword", {required: true, pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/ })} />
-                    {errors2.signupPassword && <p style={{color: 'red'}}> Please fill in a correct  sign up password </p>}
-                  </div>
-                  <div class="button input-box">
+                  {errors2.signupPassword && (
+                    <p style={{ color: "red" }}>
+                      Please fill in a correct sign up password{" "}
+                    </p>
+                  )}
+                  <div className="button input-box">
                     <input type="submit" value="Submit" />
                   </div>
-                  <div class="text sign-up-text">
-                    Already have an account? <label for="flip">Login now</label>
+                  <div className="text sign-up-text">
+                    Already have an account? <label htmlFor="flip">Login now</label>
                   </div>
                 </div>
               </form>
@@ -97,6 +170,5 @@ function Form() {
     </div>
   );
 }
-
 
 export default Form;
