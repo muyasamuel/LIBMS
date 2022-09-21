2406697685123de71e84da8737a4724d94bcb500
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-21 23:06:40 +0300

    add icons to the contents div

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,26 +1,27 @@
-import './Contents.css'
+import './Contents.css';
+import { FaAdn , FaSearch, FaBoxTissue, FaClone} from "react-icons/fa";
 
 function Contents() {
   return (
     <div className='contentsContainer'>
         <div className='contentsWrapper'>
             <div className='addContentDiv'>
-            <p>icon</p>
+            <FaAdn className='icon' />
             <h2>AddBook</h2>
             <h3> here you are given the privilege to add new book that never existed</h3>
         </div>
         <div className='searchContentDiv'>
-            <p>icon</p>
+            <FaSearch className='icon' />
             <h2>SearchFilter</h2>
             <h3> here you are given the privilege to add new book that never existed</h3>
         </div>
         <div className='issueContentDiv'>
-            <p>icon</p>
+            <FaBoxTissue className='icon'  />
             <h2>Issue Book</h2>
             <h3> here you are given the privilege to add new book that never existed</h3>
         </div>
          <div className='issuedContentDiv'>
-            <p>icon</p>
+          <FaClone className='icon' />
             <h2>Issued Books</h2>
             <h3> here you are given the privilege to add new book that never existed</h3>
         </div>
