1c71fb4808fb8b966e8b85603a9e062d5b7e3722
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-24 11:18:30 +0300

    Adding routes to content divs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Form from './components/Form';
 import Home from './components/Home';
-import Contents from './components/Contents'
+import Contents from './components/Contents';
+import AddBook from './components/AddBook';
+import IssueBook from './components/IssueBook';
+import SearchFilter from './components/SearchFilter';
+import IssuedBooks from './components/IssuedBooks';
+
 
 
 
@@ -15,6 +20,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="form" element={<Form />} />
         <Route path="contents" element={<Contents />} />
+        <Route path="contents/addbook" element={<AddBook />} />
+        <Route path="contents/issuebook" element={<IssueBook/>} />
+        <Route path="contents/edit" element={<SearchFilter />} />
+        <Route path="contents/issuedbooks" element={<IssuedBooks />} />
       </Routes>
     
   
