b7492406fa0473517728d592e73baaac54d547e0
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-24 14:40:27 +0300

    changing text on the content divs

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -28,22 +28,22 @@ function Contents() {
             <div onClick={navigateToAddbook }  className='addContentDiv'>
             <FaAdn className='icon' />
             <h2> AddBook </h2>
-            <h3> here you are given the privilege to add new book that never existed</h3>
+            <h3> Here you are given the privilege to add new book that never existed before</h3>
         </div>
         <div onClick={navigateToEdit } className='searchContentDiv'>
             <FaSearch className='icon' />
             <h2>SearchFilter</h2>
-            <h3> here you are given the privilege to add new book that never existed</h3>
+            <h3> Search for whatever book that you think its available and given an option of editing or deleting respective books </h3>
         </div>
         <div onClick={navigateToIssueBook } className='issueContentDiv'>
             <FaBoxTissue className='icon'  />
             <h2>Issue Book</h2>
-            <h3> here you are given the privilege to add new book that never existed</h3>
+            <h3> Whenever a book is issued to a student all relevant details are noted done against the book issued</h3>
         </div>
          <div onClick={navigateToIssuedBooks } className='issuedContentDiv'>
           <FaClone className='icon' />
             <h2>Issued Books</h2>
-            <h3> here you are given the privilege to add new book that never existed</h3>
+            <h3> A list of all issued books with the student details </h3>
         </div>
         </div>
         
