e238da2eb53844ebb8579f9655d490ffa25af824
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-25 16:09:14 +0300

    Adding a stop propagation event method on the modal div

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -6,9 +6,12 @@ function EditModal() {
     const { state, dispatch }  = useContext(EditContext);
 
     const { title, author,  category , amount} = state;
+
+    const onRemove = () =>  dispatch({type: 'REMOVE'});
+
   return (
-    <div className={styles.backdrop}>
-       <div className={styles.modal}> 
+    <div className={styles.backdrop} onClick={onRemove}>
+       <div onClick={(e) => e.stopPropagation()} className={styles.modal}> 
            <div className={styles.editBookWrapper}> 
         <h3> TITLE : {title}</h3>
         <h4> AUTHOR: {author}</h4>
@@ -23,7 +26,7 @@ function EditModal() {
         
       </div>
      
-      <button onClick={() => dispatch({type: 'REMOVE'})}>Done</button>
+      <button onClick={onRemove}>Done</button>
         </div> 
    
     </div>
