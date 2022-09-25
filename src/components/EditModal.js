e32d26e0fe1cf31610ff98d1136db3d615392d0a
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-09-25 16:22:04 +0300

    Install and put into action framer motion

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,36 +1,59 @@
 import { useContext } from "react";
 import { EditContext } from "../store/editContext";
-import styles from './EditModal.module.css';
+import styles from "./EditModal.module.css";
+import { motion } from "framer-motion";
 
 function EditModal() {
-    const { state, dispatch }  = useContext(EditContext);
+  const { state, dispatch } = useContext(EditContext);
 
-    const { title, author,  category , amount} = state;
+  const { title, author, category, amount } = state;
 
-    const onRemove = () =>  dispatch({type: 'REMOVE'});
+  const onRemove = () => dispatch({ type: "REMOVE" });
+
+  const dropIn = {
+    hidden: {
+      y: "-100vh",
+      opacity: 0,
+    },
+    visible: {
+      y: "0",
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: "spring",
+        damping: 25,
+        stiffness: 500,
+      },
+    },
+    exit: {
+      y: "-100vh",
+      opacity: 0,
+    },
+  };
 
   return (
     <div className={styles.backdrop} onClick={onRemove}>
-       <div onClick={(e) => e.stopPropagation()} className={styles.modal}> 
-           <div className={styles.editBookWrapper}> 
-        <h3> TITLE : {title}</h3>
-        <h4> AUTHOR: {author}</h4>
-        <h4> CATEGORY: {category}</h4>
-        <h4> AMOUNT: {amount}</h4>
-        
-        <form>
-          <h4> CHANGE AMOUNT :</h4>
-          <input type="number" />
-          <button >Submit</button>
-        </form>
-        
-      </div>
-     
-      <button onClick={onRemove}>Done</button>
-        </div> 
-   
+      <motion.div 
+      
+      onClick={(e) => e.stopPropagation()}
+       className={styles.modal}>
+        <div className={styles.editBookWrapper}>
+          <h3> TITLE : {title}</h3>
+          <h4> AUTHOR: {author}</h4>
+          <h4> CATEGORY: {category}</h4>
+          <h4> AMOUNT: {amount}</h4>
+
+          <form>
+            <h4> CHANGE AMOUNT :</h4>
+            <input type="number" />
+            <button>Submit</button>
+          </form>
+        </div>
+
+        <button onClick={onRemove}>Done</button>
+      </motion.div>
     </div>
-  )
+  );
 }
 
 export default EditModal;
