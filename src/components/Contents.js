f4a88b6f9c210873468da713a1b7d802da1e403d
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-10-08 09:28:47 +0300

    framer motion onload for content divs

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -26,13 +26,14 @@ function Contents() {
     <AnimatePresence>
       <div className="contentsContainer">
         <div className="contentsWrapper">
+
           <motion.div
             onClick={navigateToAddbook}
             className="addContentDiv"
-            initial={{ x: "-10vw", opacity: 0 }}
-            animate={{ x: "0", opacity: 1 }}
+            initial={{ y: "-50vw", opacity: 0 }}
+            animate={{ y: "0", opacity: 1 }}
             transition={{
-              duration: 0.1,
+              duration: 0.4,
               type: "spring",
               damping: 25,
               stiffness: 500,
@@ -41,24 +42,37 @@ function Contents() {
             <FaAdn className="icon" />
             <h2> AddBook </h2>
             <h3>
-              {" "}
+             
               Here you are given the privilege to add new book that never
               existed before
             </h3>
           </motion.div>
-          <div onClick={navigateToEdit} className="searchContentDiv">
+
+
+          <motion.div onClick={navigateToEdit} className="searchContentDiv"
+          initial={{ x: "50vw", opacity: 0 }}
+          animate={{ x: "0", opacity: 1 }}
+          transition={{
+            duration: 0.1,
+            type: "spring",
+            damping: 25,
+            stiffness: 500,
+          }} >
             <FaSearch className="icon" />
             <h2>SearchFilter</h2>
             <h3>
-              {" "}
+            
               Search for whatever book that you think its available and given an
               option of editing or deleting respective books{" "}
             </h3>
-          </div>
+          </motion.div>
+
+
           <motion.div onClick={navigateToIssueBook} className="issueContentDiv"
-           initial={{ x: "-10vw", opacity: 0 }}
+           initial={{ x: "-50vw", opacity: 0 }}
            animate={{ x: "0", opacity: 1 }}
            transition={{
+             
              duration: 0.1,
              type: "spring",
              damping: 25,
@@ -72,11 +86,24 @@ function Contents() {
               noted done against the book issued
             </h3>
           </motion.div>
-          <div onClick={navigateToIssuedBooks} className="issuedContentDiv">
+
+
+
+          <motion.div onClick={navigateToIssuedBooks} className="issuedContentDiv"
+          initial={{ y: "50vw", opacity: 0 }}
+          animate={{ y: "0", opacity: 1 }}
+          transition={{
+            duration: 0.2,
+            type: "spring",
+            damping: 25,
+            stiffness: 500,
+          }}>
             <FaClone className="icon" />
             <h2>Issued Books</h2>
             <h3> A list of all issued books with the student details </h3>
-          </div>
+          </motion.div>
+
+
         </div>
       </div>
     </AnimatePresence>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,42 @@
-import './Home.css';
-import { Link, useNavigate  } from 'react-router-dom';
-
+import "./Home.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
-
   const navigate = useNavigate();
 
   const navigateToFormsPage = () => {
-    navigate('/form');
-  }
+    navigate("/form");
+  };
 
   return (
-    <div>
-          <div className='homeContainer'>
+    <div className="homeWrapper">
+      <div className="homeContainer">
         <nav>
-        <h1><span>LIB</span>MS</h1>
-        <div className='actions'>
-          
-          <Link className='navBtn' to="/form">Get Started</Link>
-
-       
-        </div>
-       
+          <h1>
+            <span>LIB</span>MS
+          </h1>
+          <div className="actions">
+            <Link className="navBtn" to="/form">
+              Get Started
+            </Link>
+          </div>
         </nav>
 
         <main>
-         
-          <h1>Automate <br></br>Your <span>Library</span>  <br></br>to Fully Digital one...</h1>
-          <div className='btnDiv'>
-           <button onClick={navigateToFormsPage}    className='mainBtn'> Get Started</button>
+          <h1>
+            Automate <br></br>Your <span>Library</span> <br></br>to Fully
+            Digital one...
+          </h1>
+          <div className="btnDiv">
+            <button onClick={navigateToFormsPage} className="mainBtn">
+              Get Started
+            </button>
           </div>
-         
-
         </main>
-       
-    </div>
-    <div className='contents'>
+      </div>
       
-
     </div>
-
-    </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
