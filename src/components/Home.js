4ef2490635762c0063f25d7b2be198d4a9b3bc66
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-10-17 23:35:30 +0300

    test fetch API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,53 @@
 import "./Home.css";
-import { Link, useNavigate } from "react-router-dom";
+import {  Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import Hero from "./Hero";
 import Main from "./Main";
 import Footer from "./Footer";
 
 function Home() {
-  const navigate = useNavigate();
+  const signUpHandler = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/user/sign-up/", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Jason Sweet",
+        email: "iamsamuel6535@gmail.com",
+        password: "sammynjomo001",
+      }),
+    });
 
-  const navigateToFormsPage = () => {
-    navigate("/form");
+    response.json().then((data) => {
+      console.log(data);
+    });
   };
+  // const navigate = useNavigate();
+
+  // const navigateToFormsPage = () => {
+  //   navigate("/form");
+  // };
 
   return (
     <div className="homeWrapper">
       <div className="homeContainer">
         <nav>
           <motion.h1
-          initial={{x:-150, opacity:0}}
-          animate={{x: 0 , opacity: 1}}
-          transition={{duration: 1.2}}
+            initial={{ x: -150, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.2 }}
           >
             <span>LIB</span>MS
           </motion.h1>
 
-
-
-          <motion.div 
-            initial={{x: 150, opacity:0}}
-            animate={{x: 0 , opacity: 1}}
-            transition={{duration: 1.2}}
-          
-          className="actions">
+          <motion.div
+            initial={{ x: 150, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.2 }}
+            className="actions"
+          >
             <Link className="navBtn" to="/form">
               Get Started
             </Link>
@@ -39,18 +55,17 @@ function Home() {
         </nav>
 
         <motion.main
-        initial={{opacity: 0}}
-        whileInView={{opacity: 1}}
-        transition={{duration: 1.5, delay: 0.6}}
-        viewport={{once: true}}
-        
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.6 }}
+          viewport={{ once: true }}
         >
           <h1>
             Automate <br></br>Your <span>Library</span> <br></br>to Fully
             Digital one...
           </h1>
           <div className="btnDiv">
-            <button onClick={navigateToFormsPage} className="mainBtn">
+            <button onClick={signUpHandler} className="mainBtn">
               Get Started
             </button>
           </div>
@@ -59,7 +74,6 @@ function Home() {
       <Hero />
       <Main />
       <Footer />
-      
     </div>
   );
 }
