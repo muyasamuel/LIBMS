ccca48dce33203bc6b971d5696346ff6ef1873de
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-04 17:50:11 +0300

    Add a qoutes section in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,11 @@ import './Main.css';
 
 function Main() {
   return (
-    <div className='mainContainer'>Main</div>
+    <div className='mainContainer'>
+      <div className='quoteDivWrapper'>
+          <h1> "Libraries store the energy that fuels the imagination.They open up windows <br/> for the world and inspire as to expore and achieve , <br/>  and contribute to improving the quality of life"</h1>
+      </div>
+    </div>
   )
 }
 
