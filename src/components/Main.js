5e5bcf5515ef0f8ea990437e887b5fd5ea5ba5e6
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-05 17:23:47 +0300

    Add Quote react icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 import './Main.css';
+import { FaQuoteLeft } from "react-icons/fa";
 
 function Main() {
   return (
     <div className='mainContainer'>
       <div className='quoteDivWrapper'>
-          <h1> "Libraries store the energy that fuels the imagination.They open up windows <br/> for the world and inspire as to expore and achieve , <br/>  and contribute to improving the quality of life"</h1>
+          <h1><FaQuoteLeft /> Libraries store the energy that fuels the imagination.They open up windows <br/> for the world and inspire as to expore and achieve , <br/>  and contribute to improving the quality of life"</h1>
       </div>
     </div>
   )
