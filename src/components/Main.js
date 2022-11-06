9c3981a7124dae361701628fe65c9f3bf96f56b4
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-06 13:35:56 +0300

    finalise on the main sec

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,11 @@
 import './Main.css';
-import { FaQuoteLeft } from "react-icons/fa";
+import { FaQuoteLeft, FaQuoteRight  } from "react-icons/fa";
 
 function Main() {
   return (
     <div className='mainContainer'>
       <div className='quoteDivWrapper'>
-          <h1><FaQuoteLeft /> Libraries store the energy that fuels the imagination.They open up windows <br/> for the world and inspire as to expore and achieve , <br/>  and contribute to improving the quality of life"</h1>
+          <h1><FaQuoteLeft className='quote'  /> Libraries store the energy that fuels the imagination. They open up windows <br/> for the world and inspire as to expore and achieve , <br/>  and contribute to improving the quality of life <FaQuoteRight className='quote' /></h1>
       </div>
     </div>
   )
