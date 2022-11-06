fc8cc6d7080b938870865dbf7e4cc37c226dd203
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-06 16:59:19 +0300

    Adding contents to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,37 @@
-import './Footer.css'
+import './Footer.css';
+import { BsFacebook,BsInstagram,BsTwitter  } from "react-icons/bs";
+import { GiWhiteBook } from "react-icons/gi";
 
 function Footer() {
   return (
-    <div className='footerContainer'>Footer</div>
+    <div className='footerContainer'>
+      <div className='footerWrapper'> 
+       <div className='footersHeader'>
+        <h2>LIBMS</h2>
+        <p> Your Online Library...</p>
+       </div>
+
+       <div className='footersLinks'>
+        <ul>
+          <li>Home</li>
+          <li>FAQs</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+        </ul>
+       </div>
+
+       < div className='footersSocialIcons'>
+        <BsFacebook className='icon' />
+         <BsInstagram className='icon' />
+         <BsTwitter className='icon' />
+         <BsFacebook className='icon' />
+       </div>
+
+       <div className='textFooter'>
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi</p>
+       </div>
+       </div>
+    </div>
   )
 }
 
