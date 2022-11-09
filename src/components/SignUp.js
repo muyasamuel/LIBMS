1e53375401bcb9bddf44c0146a6dff63b1852891
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-09 16:32:00 +0300

    Handling form errors

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,6 +12,8 @@ function SignUp() {
   const [formState, setFormState] = useState(defaultFormState);
   
 
+  const [success, setSuccess] = useState(false);
+
  
 
   const onChangeHandler = (field, value) => {
@@ -27,7 +29,15 @@ function SignUp() {
   };
   const handleSubmit =  (e) => {
     e.preventDefault();
-   
+    let hasErrors = handleFormValidations();
+    
+
+    if (hasErrors) {
+      setSuccess(false);
+      return;
+    }
+    setSuccess(true);
+
 
     let updatedState = {...formState};
     const { name,  emailAddress, password } = updatedState;
@@ -60,6 +70,54 @@ function SignUp() {
 
   };
 
+  const handleFormValidations = () => {
+    
+    let updatedState = { ...formState };
+    let error = false;
+
+    const { name,  emailAddress, password, confirmPassword } =
+      updatedState;
+
+    if (name.value?.length < 3) {
+      updatedState.name.error =
+        ' Name cannot be less than 3 characters';
+      error = true;
+    }
+   
+
+    const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!emailAddress?.value?.match(re)) {
+      updatedState.emailAddress.error = 'Email address is invalid';
+      error = true;
+    }
+
+    if (
+      password?.value &&
+      confirmPassword?.value &&
+      password?.value !== confirmPassword?.value
+    ) {
+      updatedState.confirmPassword.error = 'Passwords do not match';
+      error = true;
+    }
+
+    if (!password?.value) {
+      updatedState.password.error = 'Password cannot be empty';
+      error = true;
+    }
+    if (!confirmPassword?.value) {
+      updatedState.confirmPassword.error = 'Confirm Password cannot be empty';
+      error = true;
+    }
+
+    setFormState({
+      ...formState,
+      ...updatedState,
+    });
+    return error;
+  };
+ 
  
 
   return (
@@ -74,7 +132,9 @@ function SignUp() {
           value={formState?.name?.value}
           onChange={(e) => onChangeHandler('name', e.target.value)}
         /> 
-        
+        {formState?.name?.error && (
+          <small className="error">{formState.name.error}</small>
+        )}
       </div>
       <div className="form-element">
         <label className="label">Email Address</label>
@@ -86,7 +146,9 @@ function SignUp() {
          
           onChange={(e) => onChangeHandler('emailAddress', e.target.value)}
         />
-      
+         {formState?.emailAddress?.error && (
+          <small className="error">{formState.emailAddress.error}</small>
+        )}
        </div>
       <div className="form-element">
         <label className="label">Password</label>
@@ -96,7 +158,9 @@ function SignUp() {
           value={formState?.password?.value}
           onChange={(e) => onChangeHandler('password', e.target.value)}
         />
-       
+        {formState?.password?.error && (
+          <small className="error">{formState.password.error}</small>
+        )}
        
       </div>
       <div className="form-element">
@@ -109,13 +173,18 @@ function SignUp() {
           
           
         />
-         
+          {formState?.confirmPassword?.error && (
+          <small className="error">{formState.confirmPassword.error}</small>
+        )}
       
       </div>
       <button type="submit" className="button">
         Register
       </button>
-     
+      {success && (
+        <p className="success">Form as been submitted successfully!</p>
+      )}
+      
     </form>
     </div>
     
