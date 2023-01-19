0a26c11a8ac3d86377c939d641a6e7fd78f76c56
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2023-01-19 23:57:40 +0300

    Linking api to issue a book to student

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -1,16 +1,18 @@
 export const issuedBooks = [
-    {
-        studentsnName: 'samuel Njomo',
-        studentsEmail: 'iamsamuel6535@gmail.com',
-        bookTitle: 'kidagaa',
-        issuedDate: '24-12-2022',
-        returnDate: '24-01-2023'
+    {   
+        id: 1,
+        students_name: 'samuel Njomo',
+        student_email: 'iamsamuel6535@gmail.com',
+        title: 'kidagaa',
+        issued_date: '24-12-2022',
+        return_date: '24-01-2023'
     },
     {
-        studentsnName: 'Cherish Mugure',
-        studentsEmail: 'cherish@gmail.com',
-        bookTitle: 'Mstahiki Meya',
-        issuedDate: '24-12-2022',
-        returnDate: '24-01-2023'
+        id: 2,
+        students_name: 'Cherish Mugure',
+        students_email: 'cherish@gmail.com',
+        title: 'Mstahiki Meya',
+        issued_date: '24-12-2022',
+        return_date: '24-01-2023'
     }
 ]
